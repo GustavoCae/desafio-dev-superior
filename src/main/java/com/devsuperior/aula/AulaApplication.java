815b3b53cccbe589a5bcdd72050b499913ecd8be
Oniukan <gustavo.smc@hotmail.com>
2023-07-27 18:45:50 -0300

    Adição do código Locale

--- src/main/java/com/devsuperior/aula/AulaApplication.java
+++ src/main/java/com/devsuperior/aula/AulaApplication.java
@@ -10,23 +10,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class AulaApplication implements CommandLineRunner {
-
 	@Autowired
 	private ShippingService shippingService;
-
 	@Autowired
 	private OrderService orderService;
-
 	public static void main(String[] args) {SpringApplication.run(AulaApplication.class, args);
 
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Digite o código do pedido: ");
